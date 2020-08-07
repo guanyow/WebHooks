f5ce8baa32a7dd0d199e91257bf50d6e31634bfd
guanyow <69334564+guanyow@users.noreply.github.com>
2020-08-07 14:49:54 +0800

    Update MyWebHookHandler.cs

--- WebHooks/MyWebHookHandler.cs
+++ WebHooks/MyWebHookHandler.cs
@@ -31,8 +31,11 @@ namespace DeliverEatRevamp
             {
                 // Get JSON from WebHook
            //     CyberSourceHook test = new CyberSourceHook();
+                           string strTemp = HttpContext.Current.Server.MapPath("~/");
+                // Get JSON from WebHook
                 JObject data = context.GetDataOrDefault<JObject>();
-                StreamWriter sw = new StreamWriter("log1.txt", true);
+                string path = String.Format("{0}\\{1}", strTemp, "log1.txt");
+                StreamWriter sw = new StreamWriter(path, true);
                 sw.WriteLine(data);
            //     CyberSourceHook request = JsonConvert.DeserializeObject<CyberSourceHook>(data.Root.ToString());
                 if (context.Id == "i")
